3aac1d5641baaca368d282e6bfb8ddcf6b867ac5
Ilya R <rogachev.ilya@list.ru>
2023-06-24 20:36:34 +0700

    upd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { Navbar } from '/widgets/Navbar'
+// import { Navbar } from '/widgets/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
