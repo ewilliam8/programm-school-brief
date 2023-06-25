06eaa5efacdc4f4e763e305839de6aab579190c9
Ilya R <rogachev.ilya@list.ru>
2023-06-25 18:52:37 +0700

    404page, fixes

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
+import Layout from "app/layout";
+import Image from "next/image";
 
 export default function notFound() {
   return (
-    <div>
-      Страница не найдена
-    </div>
-  )
+    <Layout>
+      <div className="page404">
+        <Image
+          src="/images/general/404.png"
+          alt="Страница не найдена"
+          layout="responsive"
+          width={1080}
+          height={1080}
+        />
+      </div>
+    </Layout>
+  );
 }
-
